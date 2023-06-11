c10049ac85fdced698a97b0f4628357af7e7bb64
Grant Birkinbine <grantbirki@github.com>
2023-06-11 14:09:54 -0600

    import ersilia

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Union
+from ersilia import ErsiliaModel
 
 from fastapi import FastAPI
 
@@ -8,3 +9,15 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/predict")
+def predict_ersilia():
+    input = [
+        "C1=C(SC(=N1)SC2=NN=C(S2)N)[N+](=O)[O-]",
+        "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O"
+    ]
+    
+    with ErsiliaModel("eos2v11") as mdl:
+        results = mdl.predict(input)
+
+    return results
