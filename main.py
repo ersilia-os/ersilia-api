ff922872591fc7fbbf22530449218204306cf309
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-06-12 16:14:03 +0200

    added input and model_id parameters to apis, changed from get to post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from typing import Union
+from typing import Union, List
+from pydantic import BaseModel
 from ersilia import ErsiliaModel
 
 from fastapi import FastAPI
@@ -6,18 +7,29 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+class ModelId(BaseModel):
+    model_id: str
+
+
+class InputData(BaseModel):
+    input: List[str]
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Welcome": "To Ersilia!"}
 
-@app.get("/predict")
-def predict_ersilia():
-    input = [
-        "C1=C(SC(=N1)SC2=NN=C(S2)N)[N+](=O)[O-]",
-        "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O"
-    ]
-    
-    with ErsiliaModel("eos2v11") as mdl:
-        results = mdl.predict(input)
 
+@app.post("/info")
+def info_ersilia(model_id: ModelId):
+    with ErsiliaModel(model_id) as mdl:
+        info = mdl.info()
+    return info
+
+
+@app.post("/run")
+def run_ersilia(model_id: ModelId, input_data: InputData):
+    # input_data = ["C1=C(SC(=N1)SC2=NN=C(S2)N)[N+](=O)[O-]", "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O"]
+    with ErsiliaModel(model_id) as mdl:
+        results = mdl.run(input_data)
     return results
