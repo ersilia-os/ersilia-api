7ad37ef4f3392a15411015e1bf3b7a0f69e29b42
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-09-04 20:11:55 +0200

    wip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Union, List
 from pydantic import BaseModel
 from ersilia import ErsiliaModel
+from ersilia.hub.fetch import ModelFetcher
+from ersilia.hub.content import ModelCatalog
 
 from fastapi import FastAPI
 
@@ -15,9 +17,38 @@ class InputData(BaseModel):
     input: List[str]
 
 
+class IntegerData(BaseModel):
+    number: int
+
+
 @app.get("/")
 def read_root():
     return {"Welcome": "To Ersilia!"}
+
+
+@app.get("/catalog")
+def catalog_ersilia():
+    mc = ModelCatalog()
+    return mc.get()
+
+
+@app.post("/serve")
+def serve_ersilia(model_id: ModelId):
+    return
+
+@app.get("/close")
+def close_ersilia():
+    return
+
+
+@app.get("/current")
+def current_ersilia():
+    return ["foo"]
+
+@app.post("/fetch")
+def fetch_ersilia(model_id: ModelId):
+    mf = ModelFetcher()
+    mf.fetch(model_id)
 
 
 @app.post("/info")
@@ -27,9 +58,15 @@ def info_ersilia(model_id: ModelId):
     return info
 
 
+@app.post("/example")
+def example_ersilia(model_id: ModelId, n_samples: IntegerData):
+    with ErsiliaModel(model_id) as mdl:
+        samples = mdl.example()
+    return samples
+
+
 @app.post("/run")
 def run_ersilia(model_id: ModelId, input_data: InputData):
-    # input_data = ["C1=C(SC(=N1)SC2=NN=C(S2)N)[N+](=O)[O-]", "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O"]
     with ErsiliaModel(model_id) as mdl:
         results = mdl.run(input_data)
     return results
